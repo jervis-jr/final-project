43ac8aa92a820ef64a9c5be58f3fc279a8eaea52
jervis-jr <giorgicercvadze82@gmail.com>
2025-04-05 01:21:59 +0400

    add responsive for 1024 px and finish whole project

--- main.js
+++ main.js
@@ -33,3 +33,98 @@ window.addEventListener("scroll", () => {
     expandablePhotoshopy.style.width = `${PhotoshopyNewWidth}px`;
   }
 });
+
+// recomendation slider
+
+let recommendations = document.querySelectorAll(".recoWrapper");
+let choosers = document.querySelectorAll(".recochooser-div");
+
+choosers.forEach((chooser, i) => {
+  chooser.addEventListener("click", () => {
+    recommendations[i].classList.remove("dn");
+    chooser.classList.add("active");
+    removeOtherClasses(i);
+  });
+});
+
+function removeOtherClasses(index) {
+  recommendations.forEach((recommendation, i) => {
+    if (i != index) {
+      recommendation.classList.add("dn");
+    }
+  });
+
+  choosers.forEach((chooser, i) => {
+    if (i != index) {
+      chooser.classList.remove("active");
+    }
+  });
+}
+
+// latest project chooser
+
+let keys = document.querySelectorAll(".aside-p");
+let projects = document.querySelectorAll(".project-card");
+
+keys.forEach((key, i) => {
+  key.addEventListener("click", () => {
+    key.classList.add("active-p");
+    if (key.innerHTML == "All") {
+      projects.forEach((project, j) => {
+        project.classList.remove("dn");
+      });
+    } else {
+      projects.forEach((project, j) => {
+        project.classList.remove("dn");
+      });
+      projects.forEach((project, j) => {
+        if (i - 1 != j) {
+          project.classList.add("dn");
+        }
+      });
+    }
+    keys.forEach((key, z) => {
+      if (z != i) {
+        key.classList.remove("active-p");
+      }
+    });
+  });
+});
+
+// POST HTTP Request
+const firstName = document.querySelector("#name");
+const email = document.querySelector("#email");
+const web = document.querySelector("#web");
+const message = document.querySelector("#message");
+const btn = document.querySelector("#submit");
+const modal = document.querySelector(".modal");
+const modalBtn = document.querySelector(".modal-btn");
+
+btn.addEventListener("click", () => {
+  const url = "https://borjomi.loremipsum.ge/api/send-message";
+  const data = {
+    name: firstName.value,
+    email: email.value,
+    website: web.value,
+    message: message.value,
+  };
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      modal.classList.remove("dn");
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
+
+modalBtn.addEventListener("click", () => {
+  modal.classList.add("dn");
+});
